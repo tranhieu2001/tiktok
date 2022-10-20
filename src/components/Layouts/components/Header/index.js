e6cbcf34558a5cc9c545a7789d92c7d246a2c806
tranhieu <tranhieu1892001@gmail.com>
2022-10-20 22:42:17 +0700

    modified:   src/components/Layouts/components/Header/index.js 	new file:   src/components/Popper/Menu/Header.js 	modified:   src/components/Popper/Menu/Menu.module.scss 	modified:   src/components/Popper/Menu/Menuitem.js 	modified:   src/components/Popper/Menu/index.js

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -25,6 +25,21 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'language',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -45,6 +60,14 @@ function Header() {
       setSearchResult([])
     }, 0)
   }, [])
+
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case 'language':
+        break
+      default:
+    }
+  }
 
   return (
     <header className={cx('wrapper')}>
@@ -84,7 +107,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Login</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
