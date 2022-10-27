fe51f478bdf99129f79bc0e3b09c483d6d45198f
tranhieu <tranhieu1892001@gmail.com>
2022-10-27 22:06:27 +0700

    modified:   .gitignore 	new file:   src/components/AccountItem/AccountItem.js 	modified:   src/components/AccountItem/index.js 	new file:   src/components/Button/Button.js 	modified:   src/components/Button/index.js 	new file:   src/components/GlobalStyles/GlobalStyles.js 	modified:   src/components/GlobalStyles/index.js 	new file:   src/components/Icons/Icons.js 	modified:   src/components/Icons/index.js 	new file:   src/components/Popper/Menu/Menu.js 	modified:   src/components/Popper/Menu/index.js 	new file:   src/layouts/DefaultLayout/DefaultLayout.js 	modified:   src/layouts/DefaultLayout/index.js 	new file:   src/layouts/HeaderOnly/HeaderOnly.js 	modified:   src/layouts/HeaderOnly/index.js 	new file:   src/layouts/components/Header/Header.js 	modified:   src/layouts/components/Header/index.js 	new file:   src/layouts/components/Search/Search.js 	modified:   src/layouts/components/Search/index.js 	new file:   src/layouts/components/Sidebar/SideBar.js 	modified:   src/layouts/components/Sidebar/index.js 	modified:   src/routes/index.js 	new file:   src/routes/routes.js

---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,102 @@
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import HeadlessTippy from '@tippyjs/react/headless'
+import classNames from 'classnames/bind'
+import { useEffect, useRef, useState } from 'react'
+
+import * as searchServices from '~/services/searchService'
+import AccountItem from '~/components/AccountItem'
+import { SearchIcon } from '~/components/Icons'
+import { Wrapper as PopperWrapper } from '~/components/Popper'
+import { useDebounce } from '~/hooks'
+import styles from './Search.module.scss'
+const cx = classNames.bind(styles)
+
+function Search() {
+  const [searchValue, setSearchValue] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const [showResult, setShowResult] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  const inputRef = useRef()
+
+  const debounced = useDebounce(searchValue, 800)
+
+  const handleClear = () => {
+    setSearchValue('')
+    setSearchResult([])
+    inputRef.current.focus()
+  }
+
+  const handleHideResult = () => {
+    setShowResult(false)
+  }
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value
+
+    if (!searchValue.startsWith(' ')) {
+      setSearchValue(searchValue)
+    }
+  }
+
+  useEffect(() => {
+    if (!debounced.trim()) {
+      setSearchResult([])
+      return
+    }
+
+    const fetchApi = async () => {
+      setLoading(true)
+
+      const result = await searchServices.search(debounced)
+      setSearchResult(result)
+
+      setLoading(false)
+    }
+
+    fetchApi()
+  }, [debounced])
+  return (
+    <div>
+      <HeadlessTippy
+        visible={showResult && searchResult.length > 0}
+        interactive
+        render={(attrs) => (
+          <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx('search-title')}>Accounts</h4>
+              {searchResult.map((result) => (
+                <AccountItem key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
+        )}
+        onClickOutside={handleHideResult}
+      >
+        <div className={cx('search')}>
+          <input
+            ref={inputRef}
+            value={searchValue}
+            placeholder="Search account and video"
+            spellCheck={false}
+            onChange={handleChange}
+            onFocus={() => setShowResult(true)}
+          />
+          {!!searchValue && !loading && (
+            <button className={cx('clear')} onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+
+          {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+          <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+            <SearchIcon />
+          </button>
+        </div>
+      </HeadlessTippy>
+    </div>
+  )
+}
+
+export default Search
